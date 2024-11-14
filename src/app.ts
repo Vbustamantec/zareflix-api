1a15a6a62b049ccce3c8bec31589d421e8eed791
Victor Bustamante Cera <victorbust.c@gmail.com>
2024-11-14 13:56:36 -0400

    [fix]: adjusted default port

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.get("/", async (_req: Request, res: Response) => {
 	try {
