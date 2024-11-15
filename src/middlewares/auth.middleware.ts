99398c533e4d560e6b76158d1cd8640ee67a9ae3
Victor Bustamante Cera <victorbust.c@gmail.com>
2024-11-15 11:17:42 -0400

    [feat]: allowed M2M request

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,10 +1,13 @@
 import { auth } from "express-oauth2-jwt-bearer";
 
 const issuerBaseUrl = process.env.AUTH0_ISSUER;
+if (!issuerBaseUrl) {
+	throw new Error("AUTH0_ISSUER is not defined");
+}
 const audience = process.env.AUTH0_AUDIENCE || "http://localhost:5000/";
 
 export const checkJwt = auth({
-	audience: audience,
+	audience: [audience, issuerBaseUrl],
 	issuerBaseURL: issuerBaseUrl,
 	tokenSigningAlg: "RS256",
 });
