eb1b59931fd17af8698b51f65ee371798fb0bae3
Victor Bustamante Cera <victorbust.c@gmail.com>
2024-11-15 08:55:26 -0400

    [fix]: modified middeware auth to updated packages

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,18 +1,10 @@
-import "dotenv/config";
+import { auth } from "express-oauth2-jwt-bearer";
 
-import { GetVerificationKey, expressjwt as jwt } from "express-jwt";
-import jwksRsa from "jwks-rsa";
 const issuerBaseUrl = process.env.AUTH0_ISSUER;
 const audience = process.env.AUTH0_AUDIENCE || "http://localhost:5000/";
 
-export const checkJwt = jwt({
-	secret: jwksRsa.expressJwtSecret({
-		cache: true,
-		rateLimit: true,
-		jwksRequestsPerMinute: 5,
-		jwksUri: `${issuerBaseUrl}/.well-known/jwks.json`,
-	}) as GetVerificationKey,
+export const checkJwt = auth({
 	audience: audience,
-	issuer: `${issuerBaseUrl}/`,
-	algorithms: ["RS256"],
+	issuerBaseURL: issuerBaseUrl,
+	tokenSigningAlg: "RS256",
 });
