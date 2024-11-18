5507512f9dc28d172cb174773ecae60b8f429075
Victor Bustamante Cera <victorbust.c@gmail.com>
2024-11-17 21:57:58 -0400

    [fix]: updated sync middleware for user creation

--- src/middlewares/userSync.middleware.ts
+++ src/middlewares/userSync.middleware.ts
@@ -32,17 +32,17 @@ export const syncUser = async (
 
 		if (!user) {
 			Logger.userSync({
-				action: 'create',
+				action: "create",
 				auth0Id: auth0User.sub,
-				email: email || auth0User.email,
-				nickname: nickname || auth0User.nickname,
-				success: true
-			  });
+				email: email,
+				nickname: nickname,
+				success: true,
+			});
 
 			user = await userRepo.createUser({
 				auth0Id: auth0User.sub,
-				email: auth0User.email as string,
-				nickname: auth0User.nickname as string,
+				email: email as string,
+				nickname: nickname as string,
 			});
 		} else {
 			await userRepo.updateLastLogin(auth0User.sub);
